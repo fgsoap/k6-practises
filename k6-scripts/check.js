fd7b1b640e40418ea99f60fff2d5145cdbdcaca3
fgsoap <fgsoap@gmail.com>
2020-11-25 19:38:07 +0800

    feat: add test

--- k6-scripts/check.js
+++ k6-scripts/check.js
@@ -2,10 +2,10 @@ import { check } from "k6";
 import http from "k6/http";
 
 export default function () {
-  let res = http.get("https://www.baidu.com");
+  let res = http.get("http://localhost:8000");
   console.log(res.body.length);
   check(res, {
     "is status 200": (r) => r.status === 200,
-    "body size is 227 bytes": (r) => r.body.length == 227,
+    "body size is 16 bytes": (r) => r.body.length == 16,
   });
 }
